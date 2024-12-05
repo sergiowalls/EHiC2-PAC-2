0fcce21b12b9014dcc370e7b94702719d8a51deb
Sergio Paredes <sparedess@uoc.edu>
2024-12-06 00:28:31 +0100

    Add project

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,12 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+import * as bootstrap from 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.css'
+
+/**
+ * Write any other JavaScript below
+ */
+
