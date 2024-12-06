e3a5e8d1ebcca23d78923797b24ef14aa49835c3
Sergio Paredes <sparedess@uoc.edu>
2024-12-06 11:54:17 +0100

    Add bootstrap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,8 +3,7 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
 
 /**
  * Write any other JavaScript below
